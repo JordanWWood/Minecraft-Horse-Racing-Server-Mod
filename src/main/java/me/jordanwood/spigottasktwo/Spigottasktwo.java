c66b7a4e46066d6f38a94d026acc02b1448448c3
Jordan Wood <up826858@myport.ac.uk>
2017-10-05 13:27:58 +0100

    Initial Spigot Setup

---
+++ src/main/java/me/jordanwood/spigottasktwo/Spigottasktwo.java
@@ -0,0 +1,16 @@
+package me.jordanwood.spigottasktwo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Spigottasktwo extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
